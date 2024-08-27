ec8e0e8c8b9457dbe5fad47840dfc5b75e363485
Alan Webb <webbalan91939@gmail.com>
2024-08-27 17:27:09 +0100

    Update header and add text animation

--- js/script.js
+++ js/script.js
@@ -11,4 +11,23 @@ function userScroll() {
 	});
 }
 
+// Typing Effect
+function runTypingEffect() {
+	const text = "I am Alan Webb";
+	const typingElement = document.getElementById("typing-text");
+	const typingDelay = 250;
+
+	typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+	for (let i = 0; i < text.length; i++) {
+		setTimeout(() => {
+			typingElement.textContent += text.charAt(i);
+		}, delay * i);
+	}
+}
+
+// Event listeners
 document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", runTypingEffect);
