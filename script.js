8e89643b88e7a5b01f29efddb4872ce9bcebd79a
Alan Webb <webbalan91939@gmail.com>
2024-09-15 21:00:41 +0100

    Update about section with vhs text effect

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // JS representaion from DOM
+// Navbar
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+// VHS REC counter
+const hourEl = document.getElementById("hours");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
 
 // Event listeners
 document.addEventListener("DOMContentLoaded", userScroll);
@@ -50,3 +55,23 @@ function typeText(text, typingElement, delay) {
 		}, delay * i);
 	}
 }
+
+// REC counter
+function updateCounter() {
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+
+	h = h < 10 ? "0" + h : h;
+	m = m < 10 ? "0" + m : m;
+	s = s < 10 ? "0" + s : s;
+
+	hourEl.innerText = h;
+	minuteEl.innerText = m;
+	secondEl.innerText = s;
+	setTimeout(() => {
+		updateCounter();
+	}, 1000);
+}
+
+updateCounter();
