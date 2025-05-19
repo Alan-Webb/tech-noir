27df964ba598a05dce83e56773a5dbb44b68bbbe
Alan Webb <webbalan91939@gmail.com>
2025-05-19 14:10:35 +0100

    Create and update index.jsx & update assets

---
+++ src/constants/index.jsx
@@ -0,0 +1,52 @@
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+
+export const navItems = [
+	{label: "Features", href: "#"},
+	{label: "How it works", href: "#"},
+	{label: "Pricing", href: "#"},
+	{label: "Testimonials", href: "#"},
+];
+
+export const testimonials = [
+	{
+		user: "Sarah Lin",
+		company: "Nebula Films",
+		image: user1,
+		text: "Tech-Noir's effects brought our alien world to life. Their attention to detail is unmatched.",
+	},
+	{
+		user: " Jared Mason",
+		company: "Starforge Studios",
+		image: user2,
+		text: "From eerie prosthetics to stunning creature designs, Tech-Noir delivered beyond expectations.",
+	},
+	{
+		user: "Leila Grant",
+		company: "Quantum Stageworks",
+		image: user3,
+		text: "We needed cinematic-level effects on a tight schedule, and Tech-Noir came through flawlessly.",
+	},
+	{
+		user: "Marco Del Rio",
+		company: "Titan’s Edge Productions",
+		image: user4,
+		text: "Their make-up team transformed our cast into believable mutants. Audiences were blown away!",
+	},
+	{
+		user: "Dani Thomasson",
+		company: "Crimson Orbit Entertainment",
+		image: user5,
+		text: "Tech-Noir's visionary team elevated our indie project to a professional level. They’re pure magic.",
+	},
+	{
+		user: "Rohan Steele",
+		company: "Starlight Syndicate",
+		image: user6,
+		text: "The realism in their sci-fi effects was jaw-dropping. Collaborating with them was a creative joy.",
+	},
+];
