aab7e0b57de122d44b87e01c87dd93348d6aae3f
Alan Webb <webbalan91939@gmail.com>
2025-05-21 08:40:29 +0100

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/50">
+			<div className="container px-4 m-auto relative text-sm">
+				<div className="flex justify-center items-center">
+					<div className="flex items-center flex-shrink-0">
+						<img />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
