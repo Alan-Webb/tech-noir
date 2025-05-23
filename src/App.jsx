dab350daccb565cb0212ebd654144baf16abd097
Alan Webb <webbalan91939@gmail.com>
2025-05-23 06:00:14 +0100

    Create Testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
 			<About />
+			<Testimonials />
 		</>
 	);
 };
