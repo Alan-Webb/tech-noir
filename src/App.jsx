215e85b46a2ab3cdd5f6674d31db7f49c4c50bd9
Alan Webb <webbalan91939@gmail.com>
2025-05-26 09:16:06 +0100

    Update Navbar, assets & index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Testimonials from "./components/Testimonials";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
-			<About />
+			<Features />
+			<HowItWorks />
 			<Testimonials />
 			<Contact />
 			<Footer />
