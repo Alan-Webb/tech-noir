f633239b493b67c0622e387c3a165debab0247e4
Alan Webb <webbalan91939@gmail.com>
2025-05-27 10:27:15 +0100

    Update assets & Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ const App = () => {
 	return (
 		<>
 			<Navbar />
-			<Hero />
-			<Features />
-			<HowItWorks />
-			<Testimonials />
-			<Contact />
-			<Footer />
+			<div className="max-w-7xl mx-auto pt-20 px-6">
+				<Hero />
+				<Features />
+				<HowItWorks />
+				<Testimonials />
+				<Contact />
+				<Footer />
+			</div>
 		</>
 	);
 };
