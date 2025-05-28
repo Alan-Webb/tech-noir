a423b9a650b36b2b485f1578edbd02fef491bde6
Alan Webb <webbalan91939@gmail.com>
2025-05-28 10:48:14 +0100

    Update features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,19 @@
 const Features = () => {
-	return <div>Features</div>;
+	return (
+		<div className="relative mt-20 border-b border-purple-800 min-h-[50rem]">
+			<div className="text-center">
+				<span className="bg-neutral-900 text-purple-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+					Features
+				</span>
+				<h2 className="text-2xl sm:text-4xl lg:text-5xl mt-8 lg:mt-12 tracking-wide">
+					Explore world-class{" "}
+					<span className="bg-gradient-to-r from-purple-500 to-purple-800 text-transparent bg-clip-text">
+						sci-fi and fantasy effects
+					</span>
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default Features;
