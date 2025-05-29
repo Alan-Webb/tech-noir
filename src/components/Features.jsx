7c6a11e29a72aa2ccd4c8c334d89251020305046
Alan Webb <webbalan91939@gmail.com>
2025-05-29 10:03:15 +0100

    Update Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,5 @@
+import {features} from "../constants";
+
 const Features = () => {
 	return (
 		<div className="relative mt-20 border-b border-purple-800 min-h-[50rem]">
@@ -11,6 +13,17 @@ const Features = () => {
 						sci-fi and fantasy effects
 					</span>
 				</h2>
+			</div>
+			<div className="flex flex-wrap mt-10 lg:mt-20">
+				{features.map((feature, index) => (
+					<div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+						<div className="flex">
+							<div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-purple-700 justify-center items-center rounded-full">
+								{feature.icon}
+							</div>
+						</div>
+					</div>
+				))}
 			</div>
 		</div>
 	);
