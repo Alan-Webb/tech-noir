6df9a7da7338089a15d6e3e4075524c551bea690
Alan Webb <webbalan91939@gmail.com>
2025-05-30 08:40:09 +0100

    Update assets

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,8 +1,8 @@
+import {Video} from "lucide-react";
+import howItWorksImg from "../assets/how-it-works.jpg";
 
 const HowItWorks = () => {
-  return (
-    <div>HowItWorks</div>
-  )
-}
+	return <div>HowItWorks</div>;
+};
 
-export default HowItWorks
+export default HowItWorks;
